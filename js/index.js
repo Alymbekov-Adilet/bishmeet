8594a71931b826a468b05a7a914bff76339f1cf5
Adilet Alymbekov <webfrontendadi@gmail.com>
2020-06-24 22:28:08 +0600

    addedd new upadte to template

---
+++ js/index.js
@@ -0,0 +1,71 @@
+//modal
+$(".gamburger_img").on("click", function () {
+  $(".modal_gamburger").slideDown("slow");
+  $(".gamburger_menu_mobile").css({ display: "none" });
+});
+
+$(".close").on("click", function () {
+  $(".modal_gamburger").slideUp("slow");
+  $(".gamburger_menu_mobile").css({ display: "flex" });
+});
+
+//carousel-1
+$(document).ready(function () {
+  $(".first").owlCarousel({
+    items: 3,
+    loop: true,
+    autoWidth: true,
+    nav: true,
+  });
+  $(".second").owlCarousel({
+    items: 4,
+    loop: true,
+    autoWidth: true,
+    nav: true,
+  });
+  $(".third").owlCarousel({
+    items: 4,
+    loop: true,
+    autoWidth: true,
+    margin: 3,
+  });
+  $(".fourth").owlCarousel({
+    items: 3,
+    loop: true,
+    autoWidth: true,
+    margin: 3,
+  });
+  $(".fifth").owlCarousel({
+    items: 3,
+    loop: true,
+    autoWidth: true,
+    margin: 3,
+  });
+  $(".sixth").owlCarousel({
+    items: 3,
+    loop: true,
+    autoWidth: true,
+    margin: 3,
+  });
+  $(".owl-item img").css({
+    width: "50%",
+    marginLeft: "60px",
+  });
+
+  $(".owl-prev").css({
+    position: "absolute",
+    left: "-1%",
+    top: "30%",
+    fontSize: "30px",
+    background: "#3b4bdb",
+    width: "30px",
+  });
+  $(".owl-next").css({
+    position: "absolute",
+    right: "-1%",
+    top: "30%",
+    fontSize: "30px",
+    background: "#3b4bdb",
+    width: "30px",
+  });
+});
